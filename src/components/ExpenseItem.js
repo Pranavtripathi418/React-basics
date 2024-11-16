c8633841d2804721d55e0367bf60a1ea366e0d0e
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-11-16 12:46:36 +0530

    react basics

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return <div>
+    <div>15 August 2024</div>
+    <div>Delhi</div>
+    <div>
+        <h2>Book</h2>
+        <div>$10</div>
+    </div>
+    </div>
+}
+
+export default ExpenseItem;
