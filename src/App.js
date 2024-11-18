d613da05aa96e4e7158a97a5eabb4796f570474e
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-11-18 22:56:32 +0530

    styling and js inside jsx

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <h1>Let's Get Started  </h1>
-      <p>I am learnng React</p>
+      
       <ExpenseItem></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,19 @@
-function ExpenseItem(){
-    return <div>
-    <div>15 August 2024</div>
-    <div>Delhi</div>
-    <div>
-        <h2>Book</h2>
-        <div>$10</div>
+import "./ExpenseItem.css";
+function ExpenseItem() {
+    const ExpenseDate= new Date(2024,7,15).toISOString();
+    const ExpenseAmount= 10;
+    const ExpenseTitle = 'Book';
+    const ExpenseLocation= 'Mumbai'; 
+  return (
+    <div className="expense-item">
+      <div className="expense-item__date">{ExpenseDate}</div>
+      <div className="expense-item__location">{ExpenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{ExpenseTitle}</h2>
+        <div className="expense-item__price">${ExpenseAmount}</div>
+      </div>
     </div>
-    </div>
+  );
 }
 
 export default ExpenseItem;
