9e2556781da9905d92693de51048b75c2be21124
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-11-19 22:29:24 +0530

    Using Map Function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {  date:new Date(2024,7,15)  ,title:"Book", location:"Delhi", price:"200"},
+    {  date:new Date(2023,5,10)  ,title:"Insurance", location:"Ayodhya", price:"190"},
+    {  date:new Date(2022,5,25)  ,title:"Laptop", location:"Deoria", price:"100"},
+    {  date:new Date(2022,8,5)  ,title:"Pen", location:"Mumbai", price:"150"}
+  ]
   return (
     <div>
       <h1>Let's Get Started  </h1>
       
-      <ExpenseItem date={new Date(2024,7,15)} title="Book" location="Delhi" price="190"></ExpenseItem>
-      <ExpenseItem date={new Date(2023,5,10)} title="Insurance" location="Gorakhpur" price="102"></ExpenseItem>
-      <ExpenseItem date={new Date(2021,9,25)} title="Laptop" location="Mumbai" price="150"></ExpenseItem>
-      <ExpenseItem date={new Date(2022,8,20)} title="Gym" location="Deoria" price="100"></ExpenseItem>
+      {expenses.map((expense)=>{
+        return (
+          <ExpenseItem date={expense.date} title={expense.title} location={expense.location} price={expense.price} ></ExpenseItem>
+        )
+      }
+    )
+      }
+
     </div>
   );
 }
