ed2f1a6aa2720fdcd7ef7ff44c2c6da4d3eb9b82
pranavtripathi418 <pranavtripathi418@gmail.com>
2024-11-30 00:42:40 +0530

    creating expense form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
 const App = () => {
   const expenses = [
     {id:1,  date:new Date(2024,7,15)  ,title:"Book", location:"Delhi", price:"200"},
@@ -10,6 +12,7 @@ const App = () => {
   return (
     <div>
       <h1>Let's Get Started  </h1>
+      <NewExpense></NewExpense>
       <Expenses items={expenses}/>
       
 
